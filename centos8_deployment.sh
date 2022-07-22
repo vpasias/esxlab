992ca05664a5eb42023a938d666a4fbbf3b31b99
vpasias <52623837+vpasias@users.noreply.github.com>
2022-07-22 17:36:41 +0300

    Update centos8_deployment.sh

--- centos8_deployment.sh
+++ centos8_deployment.sh
@@ -44,9 +44,7 @@ EOF"
 ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo modprobe -r kvm_intel && sudo modprobe -a kvm_intel"
 
 ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo dnf -y update && sudo dnf install -y git" 
-ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo dnf -y install centos-release-ansible-29"
-ssh -o "StrictHostKeyChecking=no" centos@node0 'sudo sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/CentOS-SIG-ansible-29.repo'
-ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo dnf --enablerepo=centos-ansible-29 -y install ansible"
-ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo dnf module -y install python38"
+ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo yum install epel-next-release -y"
+ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo yum install ansible -y"
 
 ssh -o "StrictHostKeyChecking=no" centos@node0 "sudo reboot"
